93df61212477dad92f122db40c6b5f7931cd5af1
thomas <thomas.chalanson@sfr.fr>
2021-11-11 12:13:04 +0100

    format date and created the propTypes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,20 +1,26 @@
 import React from "react";
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import "./Card.css";
 
 export default function Card({ image, name, created_at, rate }) {
+
+  const frenchDate = new Date(created_at).toLocaleDateString();
+
   return (
     <div className="card-container">
       <div className="card">
         <img src={image} alt={name} />
         <div className="card-Nane">{name}</div>
-        <h3>{created_at}</h3>
+        <h3>{frenchDate}</h3>
         <h4>{rate}</h4>
       </div>
     </div>
   );
 }
 
-// Card.propTypes = {
-
-// };
+Card.propTypes = {
+  created_at: PropTypes.instanceOf(Date).isRequired,
+  image: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  rate: PropTypes.number.isRequired,
+};
