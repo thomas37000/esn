e97c3b0a3db14a64673d74ef69ea23fddf7bf4ba
thomas <thomas.chalanson@sfr.fr>
2021-11-11 17:37:57 +0100

    change name by s2n_name because sql LEFT JOIN ambiguous

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,20 +2,21 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./Card.css";
 
-export default function Card({ image, name, created_at, rate }) {
+export default function Card({ images, s2n_name, created_at, rate, cities }) {
   const frenchDate = new Date(created_at).toLocaleDateString();
 
   return (
     <div className="card-container">
       <div className="card">
         <div className="divProfil">
-          <img src={image} alt={name} className="cards-image" />
+          <img src={images} alt={s2n_name} className="cards-image" />
         </div>
         <div className="card-Nane">
-          <h2>{name}</h2>
+          <h2>{s2n_name}</h2>
         </div>
         <h3>{frenchDate}</h3>
         <h4>{rate}</h4>
+        <div className="cities">{cities}</div>
       </div>
     </div>
   );
