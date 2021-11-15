dbd1db8a4b189dbf060800d144b6a800a50bc3ed
thomas <thomas.chalanson@sfr.fr>
2021-11-15 17:36:56 +0100

    add new proptypes

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./Card.css";
 
-export default function Card({ images, s2n_name, created_at, rate, citie_name }) {
+export default function Card({ images, s2n_name, created_at, rate, citie_name, techno_name }) {
   const frenchDate = new Date(created_at).toLocaleDateString();
 
   return (
@@ -11,12 +11,13 @@ export default function Card({ images, s2n_name, created_at, rate, citie_name })
         <div className="divProfil">
           <img src={images} alt={s2n_name} className="cards-image" />
         </div>
-        <div className="card-Nane">
+        <div className="card-Name">
           <h2>{s2n_name}</h2>
         </div>
         <h3>{frenchDate}</h3>
         <h4>{rate}</h4>
         <div className="cities">{citie_name}</div>
+        <div className="technos">{techno_name}</div>
       </div>
     </div>
   );
@@ -25,6 +26,8 @@ export default function Card({ images, s2n_name, created_at, rate, citie_name })
 Card.propTypes = {
   created_at: PropTypes.string.isRequired,
   image: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
+  citie_name: PropTypes.string.isRequired,
+  s2n_name: PropTypes.string.isRequired,
+  techno_name: PropTypes.string.isRequired,
   rate: PropTypes.number.isRequired,
 };
